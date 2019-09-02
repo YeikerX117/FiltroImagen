b94a8797d7f232289bbc655222b12397f6940d65
José Luis Quintero C <jlqc1772@gmail.com>
2019-09-02 12:07:05 -0500

    Update Filtro opc1.py

--- Filtro opc1.py
+++ Filtro opc1.py
@@ -11,6 +11,7 @@ def filtro(imagen, c):
                 #valor=img.item(i+1,j+1)*c-img.item(i+1,j)*c+img.item(i+1,j+1)*c
                 valor=img.item(i+1,j+1)*c-img.item(i,j)*c
             elif c>0:
+                #corregir la opreacion
                 #valor=img.item(i-1,j-1)*c-img.item(i-1,j)*c-img.item(i+1,j+1)*c
                 valor=img.item(i-1,j-1)*c-img.item(i,j)*c
             if valor>255:
@@ -20,6 +21,7 @@ def filtro(imagen, c):
             imagen.itemset((i,j), valor)
     return imagen
 img=cv2.imread("E:\INGENIERIA DE SISTEMAS\Ciencia de datos\Ejercicio Filtro\Imagenes\Prueba.png",0)
+#Colocar for anidado para recibir matrices
 matriz=[[5,1,2],[1,150,1],[0,-100,0]]
 mascara=np.sum(matriz)/float(9)
 prueba=filtro(img, mascara)
