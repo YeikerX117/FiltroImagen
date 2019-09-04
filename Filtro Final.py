98ec9226dddd83a3e7432e5a3173b1c14485c35d
YeikerX117 <52164044+YeikerX117@users.noreply.github.com>
2019-09-03 20:29:35 -0500

    Add files via upload

---
+++ Filtro Final.py
@@ -0,0 +1,30 @@
+#Filtro laplaciano a imagen "ciclica" sin borde
+import cv2
+import numpy as np
+#Arreglos de las imagenes numpy array 
+def filtro(imagen, c, s):
+    #Mismas dimensiones que recibe de una imagen 
+    #Longitud columnas filas 
+    imagen=np.zeros((img.shape[0], img.shape[1]),dtype=np.int)
+    for i in range (1, 2*(img.shape[0])):
+        for j in range(1, 2*(img.shape[1])):
+            f=i%img.shape[0]-1
+            t=j%img.shape[1]-1
+            valor=img.item(f-1,t-1)*c[0][0]+img.item(f-1,t)*c[0][1]+img.item(f-1,t+1)*c[0][2]+img.item(f,t-1)*c[1][0]+img.item(f,t)*c[1][1]+img.item(f,t+1)*c[1][2]+img.item(f+1,t-1)*c[2][0]+img.item(f+1,t)*c[2][1]+img.item(f+1,t+1)*c[2][2]
+            #valor=img.item(i-1,j-1)*c[0][0]+img.item(i-1,j)*c[0][1]+img.item(i-1,j+1)*c[0][2]+img.item(i,j-1)*c[1][0]+img.item(i,j)*c[1][1]+img.item(i,j+1)*c[1][2]+img.item(i+1,j-1)*c[2][0]+img.item(i+1,j)*c[2][1]+img.item(i+1,j+1)*c[2][2]
+            valorFinal=valor/float(9)
+            if valorFinal>255:
+                valorFinal=255
+            elif valorFinal<0:
+                valorFinal=0
+            imagen.itemset((f,t), valorFinal)
+    return imagen
+img=cv2.imread("E:\INGENIERIA DE SISTEMAS\Ciencia de datos\Ejercicio Filtro\Imagenes\Prueba.png",0)
+#Colocar for anidado para recibir matrices
+mascara=np.zeros((3,3))
+for i in range(3):
+    for j in range(3):
+        mascara[i,j]=int(input("Ingrese el dato ["+str(i)+","+str(j)+"] de la matriz: "))
+suma=mascara.sum()
+prueba=filtro(img, mascara, suma)
+cv2.imwrite("E:\INGENIERIA DE SISTEMAS\Ciencia de datos\Ejercicio Filtro\Imagenes\Prueba1.png", prueba)
